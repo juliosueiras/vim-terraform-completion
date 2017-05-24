edafa67080a2b4415f72236b94bf6e692372759c
Julio Tain Sueiras <juliosueiras@gmail.com>
2017-05-24 16:12:36 -0400

    Update all attribute with type and complete set

--- dissect.rb
+++ dissect.rb
@@ -6,6 +6,26 @@ Dir.glob("./provider_json/*.json").each do |o|
   provider = o.split('/')[-1].split('.json')[0]
   schema_json = JSON.parse(File.read("./schema_json/#{provider}.json"))
 
+  def parse_items_attr(items)
+    resources = []
+    if items != nil
+      items.each do |i|
+          item = { 'word': i[0], 'kind': "#{i[1][0]['value'].match(/Type(.*)/).captures[0]}" }
+          if i[1][-1]['value'].class == Hash
+            item[:kind] += "(B)"
+            item[:subblock] = parse_items(i[1][-1]['value'])
+          end
+          resources.push(item)
+      end
+    end
+
+    if resources.select{|i| i['word'] == 'id' }.length == 0 
+      resources.push({ 'word': 'id', 'kind': 'String' })
+    end
+
+    return resources
+  end
+
   def parse_items(items)
     resources = []
     if items != nil
@@ -60,6 +80,21 @@ Dir.glob("./provider_json/*.json").each do |o|
       value['arguments'] = parse_arrays(value['arguments'],parse_items(schema_json['resources']["#{provider}_#{key}"]))
     end
 
+    if value['attributes'] != nil and not value["attributes"].all? {|i| i == nil }
+      if key == 'vroute_entry'
+        key = key[1..-1]
+      end
+
+      value['attributes'] = parse_arrays(value['attributes'],parse_items_attr(schema_json['resources']["#{provider}_#{key}"]))
+    else
+      value["attributes"] = []
+      if key == 'vroute_entry'
+        key = key[1..-1]
+      end
+
+      value['attributes'] = parse_arrays(value['attributes'],parse_items_attr(schema_json['resources']["#{provider}_#{key}"]))
+    end
+
     schema_json['resources'].delete("#{provider}_#{key}")
 
   end
@@ -94,6 +129,21 @@ Dir.glob("./provider_json/*.json").each do |o|
       value['arguments'] = parse_arrays(value['arguments'],parse_items(schema_json['data-sources']["#{provider}_#{key}"]))
     end
 
+    if value['attributes'] != nil and not value["attributes"].all? {|i| i == nil }
+      if key == 'vroute_entry'
+        key = key[1..-1]
+      end
+
+      value['attributes'] = parse_arrays(value['attributes'],parse_items_attr(schema_json['data-sources']["#{provider}_#{key}"]))
+    else
+      value["attributes"] = []
+      if key == 'vroute_entry'
+        key = key[1..-1]
+      end
+
+      value['attributes'] = parse_arrays(value['attributes'],parse_items_attr(schema_json['data-sources']["#{provider}_#{key}"]))
+    end
+
     schema_json['data-sources'].delete("#{provider}_#{key}")
 
   end
@@ -107,7 +157,6 @@ Dir.glob("./provider_json/*.json").each do |o|
         end
 
         original_json['datas'][key.split("#{provider}_")[1]] = { 'arguments': parse_items(value) }
-        puts key
       end
     end
   end
